31e7700dfc63ceeb54f21562252caad0788d9fef
dmr <brickdamik1996@gmail.com>
2021-12-06 19:58:27 +0300

    hw8 1&3

--- src/main/java/qu/brick/spring/web/controllers/ProductController.java
+++ src/main/java/qu/brick/spring/web/controllers/ProductController.java
@@ -6,6 +6,7 @@ import qu.brick.spring.web.services.ProductService;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -13,32 +14,53 @@ public class ProductController {
 
     private ProductService productService;
 
+    private List<Product> products;
+    private Integer page = 0;
+
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
     //http://localhost:8090/app/index.html
     //http://localhost:8090/app
+//    @GetMapping("/products")
+//    public List<Product> getAllProducts() {
+//        products = productService.getAllProducts();
+//        return products;
+//    }
+
     @GetMapping("/products")
-    public List<Product> getAllProducts() {
-        return productService.getAllProducts();
+    public List<Product> getAllProducts(@RequestParam(defaultValue = "0") Integer page) {
+        products = productService.getAllProducts();
+        page -= 1;
+        if (page >= 0) {
+            this.page = page;
+        }
+        if (products.size() < this.page * 10 + 10) {
+            return products.subList(this.page * 10, products.size());
+        } else {
+            return products.subList(this.page * 10, this.page * 10 + 10);
+        }
+    }
+
+    @GetMapping("/products/pages")
+    public List<Integer> getPages() {
+        products = productService.getAllProducts();
+        List<Integer> pages = new  ArrayList<>();
+        for (int i = 1; i <= ((products.size() - 1) / 10 % 10) + 1; i++) {
+            pages.add(i);
+        }
+        return pages;
     }
 
     @GetMapping("/products/{id}")
-    public Product getStudentById(@PathVariable Long id) {
-        return productService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Student not found, id: " + id));
+    public Product getProductById(@PathVariable Long id) {
+        return productService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product not found, id: " + id));
     }
 
     @GetMapping("/products/delete/{id}")
     public void deleteById(@PathVariable Long id) {
         productService.deleteById(id);
-    }
-
-    //    @PostMapping("/products/add")
-    @GetMapping("/products/add")
-    public List<Product> addNewProduct(@RequestParam String title, @RequestParam BigInteger cost) {
-        productService.save(new Product(title, cost));
-        return productService.getAllProducts();
     }
 
     @GetMapping("/products/change_cost")
@@ -47,7 +69,7 @@ public class ProductController {
     }
 
     @GetMapping("/products/cost_between")
-    public List<Product> findProductsByCostBetween(@RequestParam(defaultValue = "0") BigInteger min, @RequestParam(defaultValue = "100") BigInteger max) {
+    public List<Product> findProductsByCostBetween(@RequestParam(defaultValue = "0") BigInteger min, @RequestParam(defaultValue = "1000000") BigInteger max) {
         return productService.findByCostBetween(min, max);
     }
 
